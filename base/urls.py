1c7ab997ed8d6228a7750f4c2ffc28f689027893
root <root@DESKTOP-OIAQNEB.localdomain>
2022-07-02 23:50:45 -0500

    02072022

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
     path('', views.home, name="home"),
     path('kb/<str:pk>/', views.kb, name="kb"),
     path('create-kb/', views.createKb, name="create-kb"),
--- base/views.py
+++ base/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from  .models import Kb, Category
 from .forms import KbForm
 
 
 
 def loginPage(request):
+    page = 'login'
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -26,7 +29,7 @@ def loginPage(request):
             return redirect('home')
         else:
              messages.error(request, 'Username or Password no existe')
-    context= {}
+    context= {'page': page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
@@ -34,6 +37,19 @@ def logoutUser(request):
     return redirect('home')
 
 
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+    return render(request, 'base/login_register.html', {'form': form})
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -52,7 +68,8 @@ def home(request):
 
 def kb(request, pk):
     kb = Kb.objects.get (id=pk)  
-    context = {'kb': kb}
+    kb_messages = kb.message_set.all()
+    context = {'kb': kb, 'kb_messages': messages}
     return render(request, 'base/kb.html', context)
 
 @login_required(login_url='login')
@@ -67,10 +84,13 @@ def createKb(request):
     context = {'form': form}
     return render(request, 'base/kb_form.html', context)
 
-
+@login_required(login_url='login')
 def updateKb(request, pk):
     kb = Kb.objects.get (id=pk)   
     form = KbForm(instance=kb)
+
+    if request.user != kb.creator:
+        return HttpResponse('No estas autorizado')
 
     if request.method == "POST":
         form = KbForm(request.POST, instance=kb)
@@ -83,6 +103,10 @@ def updateKb(request, pk):
 
 def deleteKb(request, pk):
     kb = Kb.objects.get (id=pk) 
+
+    if request.user != kb.creator:
+        return HttpResponse('No estas autorizado')   
+        
     if request.method == "POST":
         kb.delete()
         return redirect('home')
